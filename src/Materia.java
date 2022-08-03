a808bb0a7ed69cdc1225f738e6cce6725f2e94d1
JafetMorales-438 <limjafmoralesx2@gmail.com>
2022-08-02 23:38:08 -0500

    Se agregó y corrigió la documentación de la clase Materia

--- src/Materia.java
+++ src/Materia.java
@@ -1,6 +1,6 @@
 /**
- * La clase "Materia" representa una materia ordinaria o de especialidad según
- * el Instituto Tecnológico De Oaxaca (ITO)
+ * La clase "Materia" representa una materia ordinaria o de especialidad segun
+ * el plan reticular del "Instituto Tecnologico De Oaxaca" (ITO)
  * @author Limni Jafet Morales Morales (JafetD438)
  * @version 1.0 (02/08/22)
  */
@@ -13,11 +13,11 @@ public class Materia {
 
     /**
      * Contructor de la clase Materia (Sin cadena)
-     * @param nombre nombre de la Materia
-     * @param semestre Semestre de la Materia
-     * @param creditos Cantidad de creditos de la materia
-     * @param estado Estado de la Materia
-     * @param esCadena Si tiene cadena con algun otra Materia (En este constructor, es NO)
+     * @param nombre    nombre de la Materia
+     * @param semestre  Semestre de la Materia
+     * @param creditos  Cantidad de creditos de la materia
+     * @param estado    Estado de la Materia
+     * @param esCadena  Si tiene cadena con algun otra Materia (En este constructor, es NO)
      */
     public Materia(String nombre, int semestre, int creditos, int estado, boolean esCadena){
         this.name = nombre;
@@ -47,7 +47,7 @@ public class Materia {
     }
 
     /**
-     * Devuelve el nombre de la materia
+     * Devuelve el nombre de la materia.
      * @return name
      */
     public String getName() {
@@ -55,7 +55,7 @@ public class Materia {
     }
 
     /**
-     * Devuelve el semestre de la materia
+     * Devuelve el semestre en que se posiciona la Materia.
      * @return semester
      */
     public int getSemester() {
@@ -65,10 +65,10 @@ public class Materia {
     /**
      * <html>
      *     <body>
-     *         <h3>Devuelve la cantidad de créditos de la materia</h3>
-     *         <p>Una materia ordinaria tiene 4 o 5 creditos</p>
-     *         <p>Una actividad complementaria tiene 2 a 3 creditos</p>
-     *         <p>las materias de esecialidad así como sercicio social tienen 10 creditos</p>
+     *         <h3>Devuelve la cantidad de creditos de la materia.</h3>
+     *         <p>Una materia ordinaria tiene 4 o 5 creditos.</p>
+     *         <p>Una actividad complementaria tiene 2 a 3 creditos.</p>
+     *         <p>Las materias de especialidad y el servicio social tienen 10 creditos.</p>
      *     </body>
      * </html>
      * @return credits
@@ -78,13 +78,8 @@ public class Materia {
     }
 
     /**
-     * <html>
-     *     <body>
-     *         <h3>Devuelve la situación de la materia</h3>
-     *     </body>
-     * </html>
-     *
-     * @return status
+     * Devuelve la situacion de la materia.
+     * @return Situación de la Materia segun sea el caso.
      */
     public int getStatus() {
         return status;
@@ -93,35 +88,40 @@ public class Materia {
     /**
      * <html>
      *     <body>
-     *         <h3>Cambia el situación de la materia, teniendo esta 9 estados</h3>
+     *         <h3>Cambia la situacion de la Materia, teniendo esta 9 estados.</h3>
      *         <p>Estatus:</p>
      *         <ol>
-     *             <li>Materia no permitida</li>
-     *             <li>Materia posible a seleccionar</li>
-     *             <li>Cursando Materia</li>
-     *             <li>Materia Acreditada</li>
-     *             <li>Cursada sin acreditar</li>
-     *             <li>Reprobada cursando</li>
-     *             <li>A curso especial</li>
-     *             <li>Especial cursando</li>
-     *             <li>Curso especial reprobado</li>
+     *             <li>Materia no permitida.</li>
+     *             <li>Materia posible a seleccionar.</li>
+     *             <li>Cursando Materia.</li>
+     *             <li>Materia Acreditada.</li>
+     *             <li>Cursada sin acreditar.</li>
+     *             <li>Reprobada cursando.</li>
+     *             <li>A curso especial.</li>
+     *             <li>Especial cursando.</li>
+     *             <li>Curso especial reprobado.</li>
      *         </ol>
      *     </body>
      * </html>
-     * @param status situación de la materia
+     * @param status situacion de la materia.
      */
     public void setStatus(int status) {
         this.status = status;
     }
 
     /**
-     * Devuelve si la Materia tiene cadena o no
-     * @return true o false si tiene cadena o no
+     * Devuelve "true" si la Materia tiene cadena o "false" si no la tiene.
+     * @return true o false según el caso.
      */
     public boolean isCadena() {
         return isCadena;
     }
 
+    /**
+     * Devuelve el nombre de la Materia con la que es cadena
+     * o devuelve "No tiene cadena" en caso de no tener una.
+     * @return Nombre de la materia según sea el caso.
+     */
     public String getCadenaCon() {
         if(!isCadena)
             return "No tiene cadena";
