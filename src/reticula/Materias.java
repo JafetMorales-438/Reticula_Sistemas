3c4daed24b2677264046bddb2536f990efde60af
JafetMorales-438 <limjafmoralesx2@gmail.com>
2022-12-22 15:26:41 -0600

    Se corrigió completamente el archivo Materias.txt, ya es funcional nuevamente. Las clases nuevas que se añadieron con anterioridad son: Materia, Materias, Reticula, Vista y Requisitos, fue necesaria esta acción para poder separar y manejar todos los recursos, ya que serán utilizados para agregar nuevas funcionalidades. La clase Materia fue modificada para que pudiera crear todos los tipos de Materias existentes en base a los constructores que ya fueron definidos con anterioridad. Se corregió el error que se tenía que agregar un botón al final para que se pudiera visualizar el penunltimo. Se agregarán nuevas funcionalidades. El software ya no está inutilizado.

--- src/reticula/Materias.java
+++ src/reticula/Materias.java
@@ -1,20 +1,14 @@
 package reticula;
 
-import reticula.Materia;
-
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Materias extends ArrayList<Materia> {
     public Materias(){crearMaterias();}
-    /* ArrayList<reticula.Materia> */
     private void crearMaterias(){
-        String ruta = "C:\\Users\\Jafet Morales\\IdeaProjects\\Reticula_Sistemas\\src\\materias.txt";
-
-        File file = new File("materias.txt");
+        String ruta = "C:\\Users\\Jafet Morales\\IdeaProjects\\Reticula_Sistemas\\src\\reticula\\Materias.txt";
         try {
             BufferedReader reader = new BufferedReader(new FileReader(ruta));
 
@@ -23,9 +17,34 @@ public class Materias extends ArrayList<Materia> {
 
             while ((line = reader.readLine()) != null){
                 String[] s = line.split(",");
-                //add(new Materia(s[0],Integer.parseInt(s[1]),Integer.parseInt(s[2]),Integer.parseInt(s[3]),Boolean.parseBoolean(s[4]),s[5]));
-            }
 
+                if (s.length == 6){
+                    /* Representa una materia sin cadenas */
+                    add(new Materia(s[0],Integer.parseInt(s[1]),Integer.parseInt(s[2]),Integer.parseInt(s[3]),Boolean.parseBoolean(s[4]),Boolean.parseBoolean(s[5])));
+                } else if (s.length == 7) {
+                    if(s[6].equals("50") || s[6].equals("70") || s[6].equals("80")){
+                        /* Representa una materia de tipo especial (se desbloquea por porcentaje) */
+                        add(new Materia(s[0],Integer.parseInt(s[1]),Integer.parseInt(s[2]),Integer.parseInt(s[3]),Boolean.parseBoolean(s[4]),Boolean.parseBoolean(s[5]),Integer.parseInt(s[6])));
+                    } else if(s[4].equals("true")){
+                        String[] materiasP = {s[6]};
+                        String[] materiasA = {"none"};
+                        /* Representa una materia que tiene una cadena */
+                        add(new Materia(s[0],Integer.parseInt(s[1]),Integer.parseInt(s[2]),Integer.parseInt(s[3]),Boolean.parseBoolean(s[4]),Boolean.parseBoolean(s[5]),materiasP,materiasA));
+                    }
+                } else if (s.length == 8) {
+                    if (s[4].equals("true")){
+                        String[] materiasP = {s[6],s[7]};
+                        String[] materiasA = {"none"};
+                        /* Representa una materia que tiene una bi-cadena */
+                        add(new Materia(s[0],Integer.parseInt(s[1]),Integer.parseInt(s[2]),Integer.parseInt(s[3]),Boolean.parseBoolean(s[4]),Boolean.parseBoolean(s[5]),materiasP,materiasA));
+                    } else if(s[5].equals("true")){
+                        String[] materiasP = {"none"};
+                        String[] materiasA = {s[6],s[7]};
+                        /* Representa una materia que pertenece a una bi-cadena */
+                        add(new Materia(s[0],Integer.parseInt(s[1]),Integer.parseInt(s[2]),Integer.parseInt(s[3]),Boolean.parseBoolean(s[4]),Boolean.parseBoolean(s[5]),materiasP,materiasA));
+                    }
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
